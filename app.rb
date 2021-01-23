7ddeb69ebf0ef739cb39d673512cd45a3a75374c
Michael Nnaji <mnnaji3@uwo.ca>
2021-01-23 23:32:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+
+get '/' do 
+  erb :index 
+end 
+
+post '/checkout' do
+  params[:item] = session['item']
+  @session = session 
+end
+
 end
